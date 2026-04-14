b18b91cd3c28d0ec5d2bf4932bf4b69ab44388ef
pi25205-sudo <pi25205@bristol.ac.uk>
2026-04-15 00:04:52 +0100

    j

--- docs/js/entity/player.js
+++ docs/js/entity/player.js
@@ -4,14 +4,15 @@ function resetPlayer() {
     player = {
         x: WORLD_W / 2, 
         y: WORLD_H / 2, 
-        size: 30, // 这个数值控制图像显示大小，不合适可以调
-        hp: 20
+        size: 70, // 这个数值控制图像显示大小，不合适可以调
+        hp: 50
     };
 }
 
 // 【核心新增】玩家绘制函数（替换绿色圆圈）
 function drawPlayer() {
     if (!player || !playerImg) return; // 防止报错
+
 
     // 图像默认从左上角画，减去size/2让中心对齐玩家坐标
     image(
